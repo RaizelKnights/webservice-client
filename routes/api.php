9dd561a9ea3cc3ad85afb9ca4e92bac3301e113e
Irvan Fathul Huda <irvanfathul@gmail.com>
2019-01-11 10:51:48 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,45 @@
+<?php
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/login', function (Request $request) {
+    $http = new Client();
+
+    $response = $http->post('http://localhost/webservice-tugas/public/api/login', [
+        'form_params' => [
+          'grant_type' => 'password',
+          'email' => $request->email,
+          'password' => $request->password,
+        ],
+      ]);
+
+    return json_decode((string) $response->getBody(), true);
+});
+
+Route::get('/user', function (Request $request) {
+    $http = new Client();
+    $response = $http->get('http://localhost/webservice-tugas/public/api/user', [
+        'headers' => [
+            'Accept' => 'application/json',
+            'Authorization' => $request->header('Authorization'),
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
+});
